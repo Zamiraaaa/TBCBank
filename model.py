e034da3518b2589fcc9dbf01f5743223e15ea642
Zamirakhon Rustamova <143606400+Zamiraaaaaaaa@users.noreply.github.com>
2023-08-30 17:16:55 +0500

    Add files via upload

---
+++ model.py
@@ -0,0 +1,53 @@
+import pandas as pd
+teams = pd.read_csv("teams.csv")
+print(teams)
+
+
+
+teams = teams[["team", "country", "year", "athletes", "age", "prev_medals", "medals"]]
+# teams.corr()["medals"]
+
+import seaborn as sns
+sns.lmplot(x='age', y='medals', data=teams, fit_reg=True, ci=None)
+teams.plot.hist(y="medals")
+
+
+
+teams[teams.isnull().any(axis=1)].head(20)
+teams = teams.dropna()
+teams.shape
+train = teams[teams["year"] < 2012].copy()
+test = teams[teams["year"] >= 2012].copy()
+# About 80% of the data
+train.shape
+
+# About 20% of the data
+test.shape
+
+
+
+from sklearn.linear_model import LinearRegression
+
+reg = LinearRegression()
+predictors = ["athletes", "prev_medals"]
+reg.fit(train[predictors], train["medals"])
+predictions = reg.predict(test[predictors])
+predictions.shape
+test["predictions"] = predictions
+test.loc[test["predictions"] < 0, "predictions"] = 0
+test["predictions"] = test["predictions"].round()
+from sklearn.metrics import mean_absolute_error
+
+error = mean_absolute_error(test["medals"], test["predictions"])
+error
+teams.describe()["medals"]
+test["predictions"] = predictions
+test[test["team"] == "USA"]
+test[test["team"] == "IND"]
+# error_by_team = errors.groupby(test["team"]).mean()
+# medals_by_team = test["medals"].groupby(test["team"]).mean()
+# error_ratio =  error_by_team / medals_by_team
+
+#
+# import numpy as np
+# error_ratio = error_ratio[np.isfinite(error_ratio)]
